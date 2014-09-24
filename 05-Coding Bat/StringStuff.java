6c8b19cd89f9ea09b167f2cf7af1a9ca3205c40e
pooneetthaper <pooneetthaper@gmail.com>
2014-09-24 08:43:20 -0400

    work with Serena

--- 05-Coding Bat/StringStuff.java
+++ 05-Coding Bat/StringStuff.java
@@ -1,3 +1,5 @@
+/* working with Serena*/
+
 public class StringStuff{
     public String nonStart(String a, String b) {
 	return a.substring(1)+b.substring(1);
@@ -9,4 +11,27 @@ public class StringStuff{
 	if (n>21) return 2*(n-21);
 	else return 21-n;
     }
+    public boolean nearHundred(int n) {
+	int a= Math.abs(100-n);
+	int b= Math.abs(200-n);
+	if (a<=10) return true;
+	if (b<=10) return true;
+	else return false;
+    }
+
+    public int teaParty(int tea, int candy) {
+	if ((tea<5)||(candy<5)) return 0;
+	if ( (tea/candy>=2) || (candy/tea>=2) ) return 2;
+	if ( (tea>=5) || (candy>=5)) return 1;
+	else return 0;
+    }
+
+    public int teaParty(int tea, int candy) {
+	if ((tea<5)||(candy<5)) return 0;
+	if ( (tea/candy>=2) || (candy/tea>=2) ) return 2;
+	if ( (tea>=5) || (candy>=5)) return 1;
+	else return 0;
+    }
+
+
 }
