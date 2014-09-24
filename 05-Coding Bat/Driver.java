e2cffef21db88761ba781df70e433d167890d7ec
pooneetthaper <pooneetthaper@gmail.com>
2014-09-23 20:03:00 -0400

    Done

--- 05-Coding Bat/Driver.java
+++ 05-Coding Bat/Driver.java
@@ -1,11 +1,18 @@
 
 public class Driver{
     public static void main(String[] args){
-	StringStuff();
-        StringStuff a= new StringStuff();
-	StringStuff b= new StringStuff();
-        nonStart();
-	//?
+        StringStuff p= new StringStuff();
+        
+	System.out.println("a=Hello and b=There");
+	System.out.println(p.nonStart("Hello","There"));
+
+	System.out.println("a=Yolo and b=Swag");
+	System.out.println(p.makeAbba("Yolo","Swag"));
 	
+	System.out.println("n=32");
+	System.out.println(p.diff21(32));
+
+	System.out.println("n=12");
+	System.out.println(p.diff21(12));
     }
 }
