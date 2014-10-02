21b90cf766e00b2271fc58f2713e82321cabe8e8
pooneetthaper <pooneetthaper@gmail.com>
2014-10-01 22:04:03 -0400

    Triangle methods

---
+++ 07-Shapes/Shapes.java
@@ -0,0 +1,58 @@
+
+
+public class Shapes {
+
+    public String box(int r, int c){
+	String s= "";
+	int row = 0;
+	int col;
+	while (row<r){
+	    col = c;
+	    while (col>0){
+		s = s + "*";
+		col=col-1;
+	    }
+	    s=s+"\n";
+	    row=row+1;
+	}
+	return s;
+    }
+    
+    public String tri1(int h){
+	String s="";
+	int row=0;
+	int col;
+	while (row<h){
+	    col=row+1;
+	    while (col>0){
+		s = s+"*";
+		col=col-1;
+	    }
+	    s=s+"\n";
+	    row=row+1;
+	}
+	return s;
+    }
+
+    public String tri2(int h){
+	String s="";
+	int row=0;
+	int col;
+	int space;
+	while (row<h){
+	    col=row+1;
+	    space=h-col;
+	    while (space>0){
+		s = s+" ";
+		space=space-1;
+	    }
+	    while (col>0){
+		s = s+"*";
+		col=col-1;
+	    }
+	    s=s+"\n";
+	    row=row+1;
+	}
+	return s;
+    }
+}
