1c611911d95b6282d65447d76cfcf54c0a61efc6
pooneetthaper <pooneetthaper@gmail.com>
2014-11-16 14:51:25 -0500

    randomizer

--- 17-ArrayLists/Mixer.java
+++ 17-ArrayLists/Mixer.java
@@ -3,39 +3,72 @@ import java.util.*;
 
 public class Mixer{
     private int[] a;
-    private int[] result;
     private ArrayList<Integer> al=new ArrayList<Integer>();
 
-    public Mixer(){
-	a=new int[10];
-	for(int i=0;i<10;i++){
+    public Mixer(int n){
+	a=new int[n];
+	for(int i=0;i<a.length;i++){
 	    a[i]=i;
+	    al.add(i);
 	}
-	result=new int[10];
     }
 
-    public int[] Mix(){
+//________________ArrayList Stuff___________________
+
+    public void Mixarraylist(){
+	for(int i=(int)Math.ceil(Math.random()*10)+10;i>0;i--){
+	    int rand1=(int)Math.floor(Math.random()*al.size());
+	    int rand2=(int)Math.floor(Math.random()*al.size());
+	    int val=al.get(rand1);
+	    al.remove(rand1);
+	    al.add(rand2, val);
+	}
+	System.out.println(al);
+    }
+
+//__________________Array Stuff_______________________
+    public void arraytester(int[] n){
+	String s="";
+	for(int i=0;i<n.length;i++){
+	    s=s+Integer.toString(n[i]);
+	    s=s+", ";
+	}
+	System.out.println(s);
+    }
+
+    public void Mixarray(){
 	int place=0;
-	while(unSaturated()){
-	    int rand=(int)Math.ceil(Math.random()*9);
+	int[] result=new int[a.length];
+	boolean unSat=true;
+	while(unSat){
+	    int rand=(int)Math.floor(Math.random()*(a.length));
 	    if (a[rand]!=-1){
 		result[place]=a[rand];
-		place++;
+		a[rand]=-1;
+		place=place+1;
 	    }
+	    //arraytester(result);
+	    //arraytester(a);
+	    boolean c=false;
+	    for(int i=0;i<a.length;i++){
+		if (a[i]!=-1) c=true;
+		//if any place is not equal -1, its not saturated by -1's
+	    }
+	    unSat=c;
 	}
-	return result;
+        a=result;
     }
 
-    public boolean unSaturated(){
-	for(int i=0;i<10;i++){
-	    if (a[i]==i)
-		return true;
-	}
-	return false;
-    }
-
+//________________________Main____________________________
     public static void main(String[] args){
-	Mixer m=new Mixer();
-	m.Mix();
+	Mixer m = new Mixer(10);
+	System.out.println("ArrayList: ");
+	System.out.println(m.al);
+	m.Mixarraylist();
+	System.out.println("");
+	System.out.println("Array: ");
+	m.arraytester(m.a);
+	m.Mixarray();
+	m.arraytester(m.a);
     }
 }
