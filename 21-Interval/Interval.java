876fefc542a746f280335dcf5eec01a64417840e
pooneetthaper <pooneetthaper@gmail.com>
2014-12-09 22:43:14 -0500

    comparing

--- 21-Interval/Interval.java
+++ 21-Interval/Interval.java
@@ -13,13 +13,33 @@ public class Interval{
     }
 
     public Interval(){
-	int l=r.nextInt(100);// (int)Math.floor(Math.random()*101);
-	int h=l + 1 + r.nextInt(100);// l+(int)Math.ceil(Math.random()*100);
+	int l=r.nextInt(50);// (int)Math.floor(Math.random()*101);
+	int h=l + 1 + r.nextInt(50);// l+(int)Math.ceil(Math.random()*100);
 
 	low=l;
 	high=h;
 	numIntervals=numIntervals+1;
 	    
+    }
+
+    public int compareTo(Interval other) {
+	if (this.low!=other.low){
+	    if (this.low > other.low) {
+		return 1;
+	    } else {
+		return -1;
+	    }
+	} else {
+	    if (this.high != other.high){
+		if (this.high > other.high) {
+		    return 1;
+		} else {
+		    return -1;
+		}
+	    } else {
+		return 0;
+	    }
+	}   
     }
 
     public String toString(){
@@ -33,18 +53,25 @@ public class Interval{
     }
 
     public static void main(String[] args){
-	
-	Interval ival=new Interval();
-	ival.printstuff();//normal as instance
-	printstuff();//static so it works without an instance
-	Interval.printstuff();//we can also call it right off a class
-
 	Interval[] a=new Interval[10];
 	for (int i=0;i<10;i++){
 	    a[i]=new Interval();
 	}
 
 	System.out.println(Arrays.toString(a));
+	System.out.println("comparing:");
+	System.out.println("");
 	
+	for (int i=0;i<9;i++){
+	    System.out.println("a["+i+"].compareTo(a["+(i+1)+"]): "+a[i].compareTo(a[i+1]));
+	    System.out.println("");
+	}
+	
+	/*
+	Interval ival=new Interval();
+	ival.printstuff();//normal as instance
+	printstuff();//static so it works without an instance
+	Interval.printstuff();//we can also call it right off a class
+	*/
     }
 }
