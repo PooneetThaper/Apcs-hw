81acdac68f1570b19301adbb113aaaba594a6299
pooneetthaper <pooneetthaper@gmail.com>
2014-12-10 08:36:24 -0500

    interfaces

--- 21-Interval/Interval.java
+++ 21-Interval/Interval.java
@@ -1,9 +1,9 @@
 import java.io.*;
 import java.util.*;
 
-public class Interval{
+class Interval implements Comparable/*, interface2, interface 3...*/{
     private int low,high;
-    private Random r=new Random();
+    private static Random r=new Random(100);
     private static int numIntervals=0;
 
     public Interval(int l, int h){
@@ -13,8 +13,8 @@ public class Interval{
     }
 
     public Interval(){
-	int l=r.nextInt(50);// (int)Math.floor(Math.random()*101);
-	int h=l + 1 + r.nextInt(50);// l+(int)Math.ceil(Math.random()*100);
+	int h=r.nextInt(100);// (int)Math.floor(Math.random()*101);
+	int l=1+ r.nextInt(h);// l+(int)Math.ceil(Math.random()*100);
 
 	low=l;
 	high=h;
@@ -22,7 +22,8 @@ public class Interval{
 	    
     }
 
-    public int compareTo(Interval other) {
+    public int compareTo(Object other) {
+	/*
 	if (this.low!=other.low){
 	    if (this.low > other.low) {
 		return 1;
@@ -39,7 +40,17 @@ public class Interval{
 	    } else {
 		return 0;
 	    }
-	}   
+	}*/
+
+	/* cast other to the appropriate type and store in a
+	   local variable for convenience */
+	
+	Interval o= (Interval)other;
+	if (this.low == o.low){
+	    return this.high - o.high;
+	} else{
+	    return this.low - o.low;
+	}
     }
 
     public String toString(){
@@ -53,8 +64,8 @@ public class Interval{
     }
 
     public static void main(String[] args){
-	Interval[] a=new Interval[10];
-	for (int i=0;i<10;i++){
+	Interval[] a=new Interval[5];
+	for (int i=0;i<5;i++){
 	    a[i]=new Interval();
 	}
 
@@ -62,11 +73,13 @@ public class Interval{
 	System.out.println("comparing:");
 	System.out.println("");
 	
-	for (int i=0;i<9;i++){
+	for (int i=0;i<4;i++){
 	    System.out.println("a["+i+"].compareTo(a["+(i+1)+"]): "+a[i].compareTo(a[i+1]));
 	    System.out.println("");
 	}
-	
+
+	Arrays.sort(a);
+	System.out.println(Arrays.toString(a));
 	/*
 	Interval ival=new Interval();
 	ival.printstuff();//normal as instance
@@ -75,3 +88,39 @@ public class Interval{
 	*/
     }
 }
+
+/*
+  interface is a java spcification that lists methods
+
+
+ex:
+
+interface name {
+
+method signature 1;
+
+method signature 2;
+
+}
+
+(a method signature is  "public/private return-type name (params)" )
+
+
+
+
+there is built in interface:
+
+interface Comparable{
+
+public int compareTo(object other);
+
+}
+
+
+
+a class that implements an interface must define all methods in the interface
+
+
+
+you can only extend one class but you can implement multiple interfaces
+ */
